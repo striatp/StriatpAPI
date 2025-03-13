c0778e6f062e02f7cafc919ff52131735aa7ac5d
Sébastien GARCIA <sebastien.works@outlook.com>
2025-03-13 21:39:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 
 // Home route
 app.get('/', (req, res) => {
-    res.send('Welcome to my API!');
+    res.send('Welcome to my API, uh, yes.');
 });
 
 // Example route 1: Get a message
